0db61793134e4e26a578c0aef5dd5da1a075e7af
mducnguyen <m.duc.nguyen.93@gmail.com>
2021-02-25 18:25:51 +0100

    Init Commit for Project

---
+++ catalog-filter/src/main/java/de/duc/nguyen/occ/catalogfilter/CatalogFilterApplication.java
@@ -0,0 +1,13 @@
+package de.duc.nguyen.occ.catalogfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogFilterApplication.class, args);
+    }
+
+}
---
+++ catalog-filter/src/test/java/de/duc/nguyen/occ/catalogfilter/CatalogFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.duc.nguyen.occ.catalogfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
